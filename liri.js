59d382706d4b9540afb3a7b3c21b12383a843884
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-10-14 08:25:39 -0500

    complete

--- liri.js
+++ liri.js
@@ -3,98 +3,144 @@
 // movie-this
 // do-what-it-says
 
-
-// =============
-// things to do to complete homework:
-// Switch case to handle arguments in command line to display correct information
-// magically crack into the spotify object returned...
-// figure out magically the random.txt file to work
-// =============
-
-
+var input = '';
+// grabs mutliple strings from the command line
+for (var i = 3; i < process.argv.length; i++) {
+    input += " " + process.argv[i];
+}
+var command = process.argv[2];
+// console.log(input);
 
 var keys = require('./keys.js');
 var Twitter = require('twitter');
+var client = new Twitter(keys);
 var request = require('request');
 var fs = require('fs');
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
+var spotifyKeys = require('./spotify_keys');
+var spotify = new Spotify(spotifyKeys);
 
-var client = new Twitter(keys);
-// making the last 20 tweets display in node
-var params = { screen_name: 'sam16809499', count: 20 };
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-            if (!error) {
-                var data = [];
-                for (var i = 0; i < tweets.length; i++) {
-                    data.push({
-                            'Tweets: ': tweets[i].text,
-                        })
-                    // my-tweets
-                        console.log(data);
-                    }
-            }
-        });
 
-// spotify code to display artists, song name, preview link, album
-var spotify = new spotify({
-  id: '94ba0b139cec4db58ce7a7d7729099a8',
-  secret: '15a44d138fe445c5b1590b268b34f486'
-});
 
-spotify.search({ type: 'track', query: 'crash into me' }, function(error, data) {
-    if ( error ) {
-        console.log('Error occurred: ' + error);
-        console.log(data.tracks.items[0].artists[0].name);
-        return;
-    }
-var getArtistNames = function(artist) {
-  console.log(artist.name);
+function getTweets() {
+    
+    // making the last 20 tweets display in node
+    var params = { screen_name: 'sam16809499', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+
+            var data = [];
+            for (var i = 0; i < tweets.length; i++) {
+                data.push({
+                    'Tweets: ': tweets[i].text,
+                })
+            }    
+                // my-tweets
+                console.log(data);
+            
+        }
+    });
 };
-    // Do something with 'data' 
-    console.log(data);
-});
 
-// omdb api to display movie information in command line
-// Store all of the arguments in an array
-var nodeArgs = process.argv;
 
-// Create an empty variable for holding the movie name
-var movieName = "";
+function spotifyThis() {
+    // spotify code to display artists, song name, preview link, album
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
+    spotify.search({ type: 'track', query: input }, function(error, data) {
+        if (error) {
+            console.log('Error occurred: ' + error);
+            // console.log(data.tracks.items[0].artists[0].name);
+            
+            return;
+        }
 
-  if (i > 2 && i < nodeArgs.length) {
+        // Do something with 'data' 
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].preview_url);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].album.name);
+        // console.log(data);
+    });
+};
 
-    movieName = movieName + "+" + nodeArgs[i];
 
-  }
+function getMovie() {
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=40e9cece";
 
-  else {
+    request(queryUrl, function(error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            var movie = JSON.parse(body);
+            console.log('Title: ' + movie.Title);
+            console.log("Release Year: " + movie.Year);
+            console.log("IMDB Rating: " + movie.imdbRating);
+        if (movie.Rating){    
+            console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+        } else {
+            console.log("Rotten Tomatoes Ratings Unavailable");
+        }   
+            console.log('Country: ' + movie.Country);
+            console.log('Language: ' + movie.Language);
+            console.log('Plot: ' + movie.Plot);
+            console.log('Actors: ' + movie.Actors);
+        }
+    });
+};
 
-    movieName += nodeArgs[i];
 
-  }
+function randomTxt(){
+    fs.readFile('random.txt', 'utf8', function (error, data){
+        if (!error) {            
+            var split = data.split(',');
+            console.log(split);
+            command = split[0];
+            input = split[1];
+            switch (command) {
+                    case 'my-tweets':
+                        getTweets();
+                        break;
+                    case 'spotify-this-song':
+                        spotifyThis();
+                        break;
+                    case 'movie-this':
+                        getMovie();
+                        break;
+                    case 'do-what-it-says':
+                        randomTxt();
+                        break;    
+                    }
+        }
+
+    });
+};
+
+
+// switch cases for my-tweets, spotify-this-song, movie-this, do-what-it-says
+
+switch (command) {
+    case 'my-tweets':
+        getTweets();
+        break;
+    case 'spotify-this-song':
+        spotifyThis();
+        break;
+    case 'movie-this':
+        getMovie();
+        break;
+    case 'do-what-it-says':
+        randomTxt();
+        break;    
 }
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 
-request(queryUrl, function(error, response, body) {
 
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log('Title: ' + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-    console.log('Country: ' + JSON.parse(body).Country);
-    console.log('Language: ' + JSON.parse(body).Language);
-    console.log('Plot: ' + JSON.parse(body).Plot);
-    console.log('Actors: ' + JSON.parse(body).Actors);
-  }
-});
+
+
+
+
+
+
 
